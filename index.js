f7a180d3475ed575aa883cf0fca3c5ddd1034897
zvizzct <victor.puerta01@estudiant.upf.edu>
2022-09-13 00:53:16 +0200

    image almost done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const rwClient = require("./twitterClient.js");
 const octokit = require("./twitterClient.js");
+const textToImage = require("text-to-image");
+const path = require("path");
+
 const CronJob = require("cron").CronJob;
+
 let lastDate = new Date();
 
 const commit = async () => {
@@ -25,15 +29,21 @@ const processData = async (data) => {
   const { files } = await getData(url);
   const { raw_url } = files[0];
 
-  console.log(raw_url);
-
+  getCode(raw_url);
   let newDate = new Date(commit.committer.date);
   if (checkDate(newDate)) {
     getCode();
   }
 };
 
-const getCode = () => {};
+const getCode = async (raw_url) => {
+  fetch(raw_url).then(function (response) {
+    response.text().then(function (text) {
+      storedText = text;
+      generate_img(storedText);
+    });
+  });
+};
 
 const checkDate = (newDate) => {
   if (newDate > lastDate) return true;
@@ -57,3 +67,11 @@ const job = new CronJob("* * * * *", () => {
 });
 
 //job.start();
+
+const generate_img = async (storedText) => {
+  const text = storedText;
+  const dataUri = await textToImage.generate(text, {
+    debug: true,
+    debugFilename: path.join("img", `codewars.png`),
+  });
+};
